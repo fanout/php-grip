e3628f42326da0c77a654604c4fafb14b322d986
harmony7 <kats@fanout.io>
2021-08-09 01:08:36 +0900

    Encode and Decode Web Socket Events

--- src/Data/WebSockets/WebSocketEvent.php
+++ src/Data/WebSockets/WebSocketEvent.php
@@ -4,6 +4,8 @@
 namespace Fanout\Grip\Data\WebSockets;
 
 
+use Error;
+use GuzzleHttp\Psr7\AppendStream;
 use GuzzleHttp\Psr7\Utils;
 use Psr\Http\Message\StreamInterface;
 
@@ -34,5 +36,115 @@ class WebSocketEvent {
 
     public function get_content() {
         return $this->content;
+    }
+
+    // Encode the specified array of WebSocketEvent instances. The returned string
+    // value should then be passed to a GRIP proxy in the body of an HTTP response
+    // when using the WebSocket-over-HTTP protocol.
+    /**
+     * @param WebSocketEvent[] $events
+     * @return StreamInterface
+     */
+    public static function encode_events( array $events ): StreamInterface {
+        $stream = new AppendStream();
+        foreach( $events as $event ) {
+
+            $content = $event->get_content();
+            if( $content !== null ) {
+                if( $content instanceof StreamInterface ) {
+                    $content = $content->getContents();
+                }
+
+                $stream->addStream( Utils::streamFor( $event->get_type() ) );
+                $stream->addStream( Utils::streamFor( " " . strtoupper(dechex( strlen( $content ) )) . "\r\n" ) );
+                $stream->addStream( Utils::streamFor( $content ) );
+                $stream->addStream( Utils::streamFor( "\r\n" ) );
+
+            } else {
+                $stream->addStream( Utils::streamFor( $event->get_type() ) );
+                $stream->addStream( Utils::streamFor( "\r\n" ) );
+            }
+
+        }
+
+        return $stream;
+
+    }
+
+    // Decode the specified HTTP request body into an array of WebSocketEvent
+    // instances when using the WebSocket-over-HTTP protocol. A RuntimeError
+    // is raised if the format is invalid.
+    /**
+     * @param StreamInterface|string $body
+     * @return WebSocketEvent[]
+     */
+    public static function decode_events( $body ): array {
+        $events = [];
+
+        $body_is_string = !($body instanceof StreamInterface);
+        $body = Utils::streamFor( $body );
+
+        while(true) {
+            $type_line = self::read_string_until( $body, "\r\n" );
+            if( $type_line === '' ) {
+                break;
+            }
+            if( substr( $type_line, -2 ) !== "\r\n" ) {
+                throw new Error('bad format');
+            }
+            $type_line = substr( $type_line, 0, -2 );
+
+            $pos_space = strpos( $type_line, ' ' );
+            if( $pos_space !== false ) {
+                $type_string = substr( $type_line, 0, $pos_space );
+                $type_length = substr( $type_line, $pos_space + 1 );
+                $type_length = hexdec( $type_length );
+                $content = $body->read( $type_length );
+                if( $body->read(2) !== "\r\n" ) {
+                    throw new Error('bad format');
+                }
+                $e = new WebSocketEvent( $type_string, $body_is_string ? $content : Utils::streamFor( $content ) );
+
+            } else {
+                $e = new WebSocketEvent( $type_line );
+            }
+            $events[] = $e;
+        }
+
+        return $events;
+    }
+
+    public static function read_string_until( StreamInterface $stream, $needle ): string {
+
+        $pieces = str_split( $needle );
+        $pieces_len = count( $pieces );
+        $index = 0;
+
+        $buffer = '';
+
+        while( true ) {
+            $read_data = $stream->read(1);
+            if( $read_data === '' ) {
+                return $buffer;
+            }
+
+            $buffer .= $read_data;
+            if( $index > 0 ) {
+                if( $read_data === $pieces[$index] ) {
+                    $index = $index + 1;
+                    if( $index >= $pieces_len ) {
+                        return $buffer;
+                    }
+                } else {
+                    $index = 0;
+                }
+            }
+            if( $read_data === $pieces[$index] ) {
+                $index = $index + 1;
+                if( $index >= $pieces_len ) {
+                    return $buffer;
+                }
+            }
+        }
     }
 }
--- tests/Unit/WebSocketEventTest.php
+++ tests/Unit/WebSocketEventTest.php
@@ -4,6 +4,7 @@
 namespace Fanout\Grip\Tests\Unit;
 
 
+use Error;
 use Fanout\Grip\Data\WebSockets\WebSocketEvent;
 use Fanout\Grip\Tests\Utils\TestStreamData;
 use PHPUnit\Framework\TestCase;
@@ -71,5 +72,131 @@ class WebSocketEventTest extends TestCase {
         $event = new WebSocketEvent( 'type', TestStreamData::get_sample_iterator() );
         $this->assertInstanceOf( StreamInterface::class, $event->get_content() );
         $this->assertEquals( TestStreamData::$sample_iterator_values_to_string, $event->get_content()->getContents() );
+    }
+
+    /**
+     * @test
+     */
+    function shouldEncodeEvents() {
+        $events = [
+            new WebSocketEvent('TEXT', 'Hello'),
+            new WebSocketEvent('TEXT', ''),
+            new WebSocketEvent('TEXT', null),
+        ];
+
+        $events_encoded = WebSocketEvent::encode_events( $events );
+        $this->assertSame(
+            "TEXT 5\r\nHello\r\nTEXT 0\r\n\r\nTEXT\r\n",
+            $events_encoded->getContents()
+        );
+    }
+
+    /**
+     * @test
+     */
+    function shouldEncodeOpenEvent() {
+        $events = [
+            new WebSocketEvent('OPEN'),
+        ];
+
+        $events_encoded = WebSocketEvent::encode_events( $events );
+        $this->assertSame(
+            "OPEN\r\n",
+            $events_encoded->getContents()
+        );
+    }
+
+    /**
+     * @test
+     */
+    function shouldEncodeBinaryEvent() {
+        $events = [
+            new WebSocketEvent('BINARY', TestStreamData::get_sample_iterator()),
+            new WebSocketEvent('BINARY', ''),
+            new WebSocketEvent('BINARY', null),
+        ];
+
+        $events_encoded = WebSocketEvent::encode_events( $events );
+        $this->assertSame(
+            "BINARY " . strlen(TestStreamData::$sample_iterator_values_to_string) . "\r\n" . TestStreamData::$sample_iterator_values_to_string . "\r\nBINARY 0\r\n\r\nBINARY\r\n",
+            $events_encoded->getContents()
+        );
+
+    }
+
+    /**
+     * @test
+     */
+    function shouldDecodeEventWithoutContent() {
+        $body = "OPEN\r\n";
+        $events = WebSocketEvent::decode_events( $body );
+        
+        $this->assertCount( 1, $events );
+        $this->assertSame( 'OPEN', $events[0]->get_type() );
+        $this->assertNull( $events[0]->get_content() );
+    }
+
+    /**
+     * @test
+     */
+    function shouldDecodeEventWithLength() {
+        $body = "TEXT 5\r\nHello\r\n";
+        $events = WebSocketEvent::decode_events( $body );
+
+        $this->assertCount( 1, $events );
+        $this->assertSame( 'TEXT', $events[0]->get_type() );
+        $this->assertSame( 'Hello', $events[0]->get_content() );
+    }
+
+    /**
+     * @test
+     */
+    function shouldDecodeMultipleEvents() {
+        $body = "OPEN\r\nTEXT 5\r\nHello\r\nTEXT 0\r\n\r\nCLOSE\r\nTEXT\r\nCLOSE\r\n";
+        $events = WebSocketEvent::decode_events( $body );
+
+        $this->assertCount( 6, $events );
+
+        $this->assertSame( 'OPEN', $events[0]->get_type() );
+        $this->assertNull( $events[0]->get_content() );
+
+        $this->assertSame( 'TEXT', $events[1]->get_type() );
+        $this->assertSame( 'Hello', $events[1]->get_content() );
+
+        $this->assertSame( 'TEXT', $events[2]->get_type() );
+        $this->assertSame( '', $events[2]->get_content() );
+
+        $this->assertSame( 'CLOSE', $events[3]->get_type() );
+        $this->assertNull( $events[3]->get_content() );
+
+        $this->assertSame( 'TEXT', $events[4]->get_type() );
+        $this->assertNull( $events[4]->get_content() );
+
+        $this->assertSame( 'CLOSE', $events[5]->get_type() );
+        $this->assertNull( $events[5]->get_content() );
+    }
+
+    /**
+     * @test
+     */
+    function shouldDecodeEventsThrow1() {
+
+        $this->expectException( Error::class );
+
+        $body = "TEXT 5\r\n";
+        WebSocketEvent::decode_events( $body );
+
+    }
+
+    /**
+     * @test
+     */
+    function shouldDecodeEventsThrow2() {
+
+        $this->expectException( Error::class );
+
+        $body = "OPEN\r\nTEXT";
+        WebSocketEvent::decode_events( $body );
+
     }
 }
